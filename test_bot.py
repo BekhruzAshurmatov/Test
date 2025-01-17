f5d510449fb5f7e10469343939194b883f22d73a
BekhruzAshurmatov <145110827+BekhruzAshurmatov@users.noreply.github.com>
2025-01-17 15:09:46 +0500

    Update test_bot.py
    version 2.0

--- test_bot.py
+++ test_bot.py
@@ -14,3 +14,19 @@ dp = Dispatcher()
 user_data = {}
 project_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(project_dir, 'images')
+
+
+
+email = 'bekhruzashurmatov1@gmail.com'
+password = 'ElwGZZg0d4ja2b6BDyuQQNs4bJeMn0calvdmGZtV'
+
+async def eskiz_login(email, password):
+    url = "https://notify.eskiz.uz/api/auth/login"
+    payload = {'email': email,
+               'password': password}
+    files = []
+    headers = {}
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    print(response.text)
+    token = response.json()['data']['token']
+    return token
